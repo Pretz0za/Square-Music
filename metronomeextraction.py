5ea84910f8dc8c932ac91eb7d4e21b6e86947836
RopeNutter <azozfaiz66@gmail.com>
2023-05-05 18:38:54 +0300

    Added some edge cases in metronomeextraction.py

--- metronomeextraction.py
+++ metronomeextraction.py
@@ -26,12 +26,15 @@ for i in range(len(times)):
     for pattern in patterns:
         if pattern[0] < times[i]:
             continue
+        if (times[-1] - (times[i] + (pattern[1] - 1)*pattern[0]) > pattern[0]):
+            continue
         if pattern[1] > bestPattern[1]:
             bestPattern = pattern
-    if not (bestPattern[1] == 2 and bestPattern == patterns[0]):
-        for j in range(bestPattern[1]):
-            trash.append(round(times[i] + (j * bestPattern[0]), 7))
-        metronomes.append((times[i], bestPattern[0], bestPattern[1]))
+    if not bestPattern == (0, -1):
+        if not (bestPattern[1] == 2 and bestPattern == patterns[0]):
+            for j in range(bestPattern[1]):
+                trash.append(round(times[i] + (j * bestPattern[0]), 7))
+            metronomes.append((times[i], bestPattern[0], bestPattern[1]))
 
 if not len(trash) == len(times):
     for time in times:
@@ -40,5 +43,5 @@ if not len(trash) == len(times):
         metronomes.append(time, times[-1], 1)
             
 
-
+print(metronomes)
 # To deal with stand alone notes that aren't a part of any pattern, I should put all "bestPattern"s that have length 2 in a temp list and not append anything from them to trash, and I should come back to them at the end of the code to resolve them.
